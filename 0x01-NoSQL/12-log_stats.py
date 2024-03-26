fb5abcab61888bb83198d6e623ea8d64ee77eaa1
effiecancode <faitheffie25@gmail.com>
2024-03-27 01:10:37 +0300

    Nginx stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
-"""
-Write a Python script that provides some stats about Nginx logs stored in MongoDB:
-Database: logs
-Collection: nginx
-Display (same as the example):
-first line: x logs where x is the number of documents in this collection
-second line: Methods:
-5 lines with the number of documents with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order (see example below - warning: it’s a tabulation before each line)
-one line with the number of documents with:
-method=GET
-path=/status
-"""
+"""A module that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
+def print_nginx_request_logs(nginx_collection):
+    """Prints stats about Nginx request logs. """
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        req_count = len(list(nginx_collection.find({"method": method})))
+        print("\tmethod {}: {}".format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({"method": "GET", "path": "/status"})
+    ))
+    print("{} status check".format(status_checks_count))
+
+
+def run():
+    """Provides some stats about Nginx logs stored in MongoDB. """
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    print_nginx_request_logs(client.logs.nginx)
+
 
 if __name__ == "__main__":
-    """provides stats about nginx logs"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    coll = client.logs.nginx
-    print("{} logs".format(coll.count_documents({})))
-    print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = coll.count_documents({'method': method})
-        print("\tmethod {}: {}".format(method, count))
-
-    status_get = coll.count_documents({'method': 'GET', 'path': "/status"})
-    print("{} status check".format(status_get))
-    
+    run()
