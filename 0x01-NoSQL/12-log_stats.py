71deebaf1c945326256a13c6b43dc935bb06d2b4
effiecancode <faitheffie25@gmail.com>
2024-03-27 22:45:24 +0300

    nginx logs stored in mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    """Prints stats about Nginx request logs. """
+    """Prints stats about Nginx request logs """
     print("{} logs".format(nginx_collection.count_documents({})))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
@@ -18,7 +18,7 @@ def print_nginx_request_logs(nginx_collection):
 
 
 def run():
-    """Provides some stats about Nginx logs stored in MongoDB. """
+    """Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient("mongodb://127.0.0.1:27017")
     print_nginx_request_logs(client.logs.nginx)
 
